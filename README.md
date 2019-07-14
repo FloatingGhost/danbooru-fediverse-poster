8cdf3a913209f83b10b96a9b4050d766788d6947
sadposter <hannah+pleroma@coffee-and-dreams.uk>
2019-07-14 16:01:28 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Danbooru Fediverse Poster
+
+Searches danbooru for images and posts them via mastodonAPI to any
+fediverse server
+
+## Installation
+
+```bash
+pip3 install -r requirements.txt
+cp config.example.yaml config.yaml
+```
+
+and edit your config file
+
+## Running
+
+```bash
+python3 run.py
+```
---
+++ run.py
@@ -0,0 +1,52 @@
+import os
+import requests
+from mastodon import Mastodon
+from pyaml import yaml
+
+with open("config.yaml", "r") as f:
+    config = yaml.load(f)
+
+BASE_URL = config["instance"]
+
+for search in config["searches"]:
+    user_creds = "{}.user.secret".format(search["user"])
+
+    if not os.path.exists(user_creds):
+        Mastodon.create_app("{}-app".format(search["user"]),
+                api_base_url=BASE_URL,
+                to_file="{}.app.secret".format(search["user"])
+        )
+
+        mastodon = Mastodon(
+            client_id="{}.app.secret".format(search["user"]),
+            api_base_url=BASE_URL
+        )
+        mastodon.log_in(
+                search["user"],
+                search["password"],
+                to_file=user_creds
+        )
+
+    mastodon = Mastodon(
+        access_token=user_creds,
+        api_base_url=BASE_URL
+    )
+
+    r = requests.get("https://danbooru.donmai.us/posts.json?random=true&tags={}&rating=s&limit=1".format("+".join(search["query"])),
+        auth=(config["danbooru"]["username"], config["danbooru"]["password"]))
+
+    r = r.json()
+    if len(r) == 0:
+        continue
+    r = r[0]["large_file_url"]
+    u = requests.get(r)
+    with open("/tmp/danbooru.png", "wb") as f:
+        f.write(u.content)
+    img = mastodon.media_post("/tmp/danbooru.png")["id"]
+
+    mastodon.status_post(
+      ".",
+      sensitive=search.get("nsfw", False),
+      media_ids=[img],
+      visibility="public"
+    )
